034feb77d0cd97490d1151f709c02b238db05331
Kireenko Olga <kireenko86.osil@mail.ru>
2021-06-21 22:12:26 +0500

    add code

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -19,7 +19,7 @@ class MobileBankApiTestV1 {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body ("data", equalTo("some data"))
+                .body ("data", equalTo("Kireenko"))
         ;
     }
 }
