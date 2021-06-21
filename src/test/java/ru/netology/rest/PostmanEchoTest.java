7adac1b9f4cb43f32c6690fd837bbbcf203d383a
Kireenko Olga <kireenko86.osil@mail.ru>
2021-06-21 14:58:58 +0500

    add code

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnBody() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Kireenko")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body ("data", equalTo("some data"))
+        ;
+    }
+}
